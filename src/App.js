06c717eee2349cf8d34be8b59b8e9a4459bc4bd6
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-07-15 15:46:54 -0700

    Was able to update the timer but I am not being able to change the quarter when I want to.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -8,6 +8,72 @@ function App() {
   const [homeScore, HomeScore] = useState(0);
   
   const [awayScore, AwayScore] = useState(0);
+
+  // const [time, timMoves] = useStat3(00:00);
+  const [seconds, Seconds] = useState(0);
+  const [minute, Minutes] = useState(0);
+  const [quarter, quarters] =  useState(1);
+
+
+  useEffect(() => {
+    let interval = null;
+      interval = setInterval(() => {
+        Seconds(seconds => seconds + 1);
+        if (seconds > 59) {
+          Seconds(0);
+          Minutes(minute => minute + 1);
+          if(minute == 2){
+            quarters( quarter => quarter+1);
+          } if(minute == 4){
+            quarters( quarter => quarter+2);
+          } if(minute == 6){
+            quarters( quarter => quarter+3);
+          } if(minute == 6){
+            return () => clearInterval(interval);
+          } 
+
+        }
+      }, 1000);
+    return () => clearInterval(interval);
+  });
+    
+  // function Counter() {
+  //   const [count, setCount] = useState(0);
+  
+  //   useInterval(() => {
+  //     // Your custom logic here
+  //     setCount(count + 1);
+  //   }, 1000);
+
+  //   if (count >= 200){
+  //     return "null"
+  //   }
+
+  
+  //   return <h1>{count}</h1>;
+  // }
+  
+  // function useInterval(callback, delay) {
+  //   const savedCallback = useRef();
+  
+  //   // Remember the latest function.
+  //   useEffect(() => {
+  //     savedCallback.current = callback;
+  //   }, [callback]);
+  
+  //   // Set up the interval.
+  //   useEffect(() => {
+  //     function tick() {
+  //       savedCallback.current();
+  //     }
+  //     if (delay !== null) {
+  //       let id = setInterval(tick, delay);
+  //       return () => clearInterval(id);
+  //     }
+  //   }, [delay]);
+  // }
+
+
 
   return (
     <div className="container">
@@ -20,7 +86,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{minute}:{seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
